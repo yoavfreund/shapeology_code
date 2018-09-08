278bd4d16a6766e8cd3607905fdc5ed6655468f5
yoav freund <yfreund@ucsd.edu>
2018-09-08 12:45:33 -0700

    scripts for managing processing of a directory of tiles

---
+++ scripts/ProcessTiles.py
@@ -0,0 +1,38 @@
+import psutil
+from os import getpid,system
+from os.path import isfile
+from glob import glob
+from time import sleep
+from os.path import isfile
+from sys import argv
+
+
+tile_pattern=argv[1]
+print(tile_pattern)
+
+max_load=700.
+i=0
+for infile in glob(tile_pattern):
+    stem=infile[:-4]
+    lockfile=stem+'.lock'
+    if not isfile(lockfile):
+        i+=1
+        print('got lock',lockfile)
+        command='python3 run_job.py %s &'%stem
+        print(i,command,'\n')
+        system(command)
+        sleep(0.1)
+    else:
+        print('\r %s exists'%lockfile,end='')
+        continue
+
+    # Wait if load is too high
+    load=sum(psutil.cpu_percent(percpu=True))
+    print('\r %5d                            load: %6.2f'%(i,load))
+    j=0
+    while load>max_load:
+        print('\r %5d    Sleep:%3d               load: %6.2f'%(i,j,load))
+        j+=1
+        sleep(2)
+        load=sum(psutil.cpu_percent(percpu=True))
+    print('\nload low enough',load)
---
+++ scripts/run_job.py
@@ -0,0 +1,32 @@
+from sys import argv
+from os import getpid,system
+from os.path import isfile
+from time import sleep
+import datetime
+
+
+def getLock(lockfile):
+    try:
+        l=open(lockfile, 'x')
+        return l
+    except FileExistsError:
+        return None
+
+stem=argv[1]
+lockfilename=stem+'.lock'
+logfilename=stem+'.log'
+
+lockfile=getLock(lockfilename)
+if not lockfile is None:
+
+    command='python3 ../scripts/extractPatches.py %s > %s'%(stem,logfilename)
+    # put some info into the log/lock file
+    now = datetime.datetime.now()
+    print('pid=',getpid(), file=lockfile)
+    print('start time=',now.isoformat(), file=lockfile)
+    print('command=',command, file=lockfile)
+    
+    system(command)
+    sleep(1)
+else:
+    print(lockfilename,'exists, skipping')
