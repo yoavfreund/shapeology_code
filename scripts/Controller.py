58db6ce11cd0444171ecf7fe5cae9b8d3b010527
yoav freund <yfreund@ucsd.edu>
2018-09-10 20:41:59 -0700

    fix

--- scripts/Controller.py
+++ scripts/Controller.py
@@ -179,7 +179,7 @@ if __name__=="__main__":
 
         #copy results to s3
         stem='MD657-N70-2017.02.22-18.31.14_MD657_1_0208_lossless'
-        run("cd {0}; tar czf ../{1}_patches.tgz *.pkl *.log *.lock".format(local_data,stem))
+        run("cd {0}/tiles; tar czf ../{1}_patches.tgz *.pkl *.log *.lock".format(local_data,stem))
         clock('created tar file {0}/{1}_patches.tgz'.format(local_data,stem))
 
         run('aws s3 cp {0}/{1}_patches.tgz {2}/'.format(local_data,stem,stack_directory))
