967b71a12b34e0d5694af7ed824957217aedf0ec
yoav freund <yfreund@ucsd.edu>
2018-09-11 21:19:37 -0700

    added watchdog and made executable

--- scripts/watchdog.py
+++ scripts/watchdog.py
@@ -22,10 +22,12 @@ def Last_Modified(file_name):
 if __name__=='__main__':
     Recent=False
     for logfile in glob(exec_dir+'/Controller*.log'):
-        if time() - Last_Modified(logfile) <600: # allow 10 minute idle
+        gap=time() - Last_Modified(logfile)
+        if gap <600: # allow 10 minute idle
+            print(logfile,'gap is %6.1f'%gap)
             Recent=True
             break
     if(not Recent):
         command='{0}/Controller.py {1} {2} &> {0}/Controller-{3}.log &'\
         .format(exec_dir,stack,local_data,int(time()))
-    run(command)
+        run(command)
