31f940e9c01649b62ca631b14f2313881f68455c
yoav freund <yfreund@ucsd.edu>
2018-09-14 09:25:21 -0700

    fix

--- scripts/watchdog.sh
+++ scripts/watchdog.sh
@@ -3,5 +3,5 @@ export PATH =/home/ubuntu/bin:/home/ubuntu/.local/bin:/home/ubuntu/KDU7A2_Demo_A
 export LD_LIBRARY_PATH=/home/ubuntu/KDU7A2_Demo_Apps_for_Centos7-x86-64_170827/
 echo 'this is watchdog'  >> /home/ubuntu/watchdog.log
 echo $PATH  >> /home/ubuntu/watchdog.log
-echo $LD_LIBRARY_PATH
+echo $LD_LIBRARY_PATH >> /home/ubuntu/watchdog.log
 watchdog.py >> /home/ubuntu/watchdog.log
