613f4ee23899824de19993ef1d9efafaf7fe2ffc
yoav freund <yfreund@ucsd.edu>
2018-09-14 09:07:18 -0700

    fix

--- scripts/watchdog.sh
+++ scripts/watchdog.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 source /home/ubuntu/.bashrc
+echo 'this is watchdog'  >> /home/ubuntu/watchdog.log
+echo $PATH  >> /home/ubuntu/watchdog.log
 watchdog.py >> /home/ubuntu/watchdog.log
