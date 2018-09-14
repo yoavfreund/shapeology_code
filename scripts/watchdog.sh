aad00e7ad30aa59d077673a299497c56e1a4df1c
yoav freund <yfreund@ucsd.edu>
2018-09-14 09:32:56 -0700

    fix

--- scripts/watchdog.sh
+++ scripts/watchdog.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export PATH =/home/ubuntu/bin:/home/ubuntu/.local/bin:/home/ubuntu/KDU7A2_Demo_Apps_for_Centos7-x86-64_170827/:/home/ubuntu/shapeology_code/scripts/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export PATH ="/home/ubuntu/bin:/home/ubuntu/.local/bin:/home/ubuntu/KDU7A2_Demo_Apps_for_Centos7-x86-64_170827/:/home/ubuntu/shapeology_code/scripts/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
 export LD_LIBRARY_PATH=/home/ubuntu/KDU7A2_Demo_Apps_for_Centos7-x86-64_170827/
 echo 'this is watchdog'  >> /home/ubuntu/watchdog.log
 echo $PATH  >> /home/ubuntu/watchdog.log
