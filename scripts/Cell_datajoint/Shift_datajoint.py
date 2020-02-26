23b92dc03568d2b33e5aa974369011db1ca7b6ff
KuiQian <qkprince92@gmail.com>
2020-02-26 11:22:14 -0800

    debug

--- scripts/Cell_datajoint/Shift_datajoint.py
+++ scripts/Cell_datajoint/Shift_datajoint.py
@@ -92,7 +92,7 @@ class Shift(dj.Computed):
         #     # setup_upload_from_s3(feature_fp+ str(section) + '.pkl', recursive=False)
         # else:
         #     run('python {0}/Shape_shift.py {1} {2} {3}'.format(scripts_dir, stack, section, yaml_file))
-        #     run('python {0}/Shape_shift_cnn.py {1} {2}'.format(scripts_dir, stack, section))
+            run('python {0}/Shape_shift_cnn.py {1} {2}'.format(scripts_dir, stack, section))
             setup_upload_from_s3(s3_fp, recursive=False)
             # setup_upload_from_s3(feature_fp + str(section) + '.pkl', recursive=False)
             report = self.client.stat_object(self.bucket, s3_fp)
