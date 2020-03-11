ddec66447a7ae45e9aaadcb05033540e92cd462b
KuiQian <qkprince92@gmail.com>
2020-03-11 02:51:32 -0700

    inception-xgb model

--- scripts/CNN_training.py
+++ scripts/CNN_training.py
@@ -1,14 +1,11 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("structure", type=str, help="The nth group of structures")
+parser.add_argument("structure", type=str, help="The name of structure")
 args = parser.parse_args()
-stack = args.stack
-struc = args.structure
-state = args.state
+structure = args.structure
 
 import cv2
-# from cv2 import moments,HuMoments
 import pickle
 import numpy as np
 import pandas as pd
@@ -17,11 +14,10 @@ import os
 import sys
 from time import time
 from glob import glob
-from extractPatches import patch_extractor
-# from label_patch import diffusionMap
-# from patch_normalizer import normalizer
-from lib.utils import mark_contours, configuration, run
-
+import mxnet as mx
+from xgboost.sklearn import XGBClassifier
+from sklearn import metrics
+from lib.utils import run
 
 def setup_download_from_s3(rel_fp, recursive=True):
     s3_fp = 's3://mousebrainatlas-data/' + rel_fp
@@ -46,61 +42,88 @@ def setup_upload_from_s3(rel_fp, recursive=True):
     else:
         run('aws s3 cp {0} {1}'.format(local_fp, s3_fp))
 
-patch_dir = 'CSHL_patch_samples/'
-cell_dir = os.environ['ROOT_DIR'] + args.filename + '_features/'
-if not os.path.exists(cell_dir):
-    os.mkdir(cell_dir)
-cell_dir = cell_dir + stack + '/'
-if not os.path.exists(cell_dir):
-    os.mkdir(cell_dir)
-    # os.mkdir(cell_dir+'Images/')
-    # os.mkdir(cell_dir+'Properties/')
-
-cell_dir = args.filename + '_features/' + stack + '/'
-# t0=time()
-
-# assert structure
-
-for state in [state]:
-    t1 = time()
-    # savepath = cell_dir + 'Properties/' + structure + '/'
-    # img_path = cell_dir + 'Images/' + structure + '/'
-    savepath = cell_dir + structure + '/'
-    pkl_out_file = savepath + stack + '_' + structure + '_' + state + '.pkl'
-    # img_out_file = img_path+stack+'_'+structure+'_'+state+'_images.pkl'
-    if os.path.exists(os.environ['ROOT_DIR'] + pkl_out_file):
-        print(structure + '_' + state + ' ALREADY EXIST')
-        continue
-    else:
-        if not os.path.exists(os.environ['ROOT_DIR'] + savepath):
-            os.mkdir(os.environ['ROOT_DIR'] + savepath)
-            # os.mkdir(img_path)
-
-    if structure == '7nn':
-        structure = '7n'
-
-    if state == 'positive':
-        setup_download_from_s3(patch_dir + structure)
-        patches = [dir for dir in glob(os.environ['ROOT_DIR'] + patch_dir + structure + '/*')]
-    else:
-        setup_download_from_s3(patch_dir + structure + '_surround_500um_noclass')
-        patches = [dir for dir in
-                   glob(os.environ['ROOT_DIR'] + patch_dir + structure + '_surround_500um_noclass/*')]
-
-    features = []
-
+def extract_cnn_features(stack, structure, batch_size, model):
+    patch_dir = os.path.join('CSHL_patch_samples', stack)
+    setup_download_from_s3(os.path.join(patch_dir, structure))
+    setup_download_from_s3(os.path.join(patch_dir, structure + '_surround_500um_noclass'))
+    patches = [dir for dir in glob(os.environ['ROOT_DIR'] + patch_dir + '/' + structure + '/*')]
     n_choose = min(len(patches), 1000)
     indices_choose = np.random.choice(range(len(patches)), n_choose, replace=False)
     patches = np.array(patches)
-    patches = patches[indices_choose]
+    patches_posi = patches[indices_choose]
+    n_pos = len(patches_posi)
 
-    count = len(features)
-    print(structure + '_' + state, count)
-    #         cells.to_pickle(pkl_out_file)
-    #         images.to_pickle(img_out_file)
-    pickle.dump(features, open(os.environ['ROOT_DIR'] + pkl_out_file, 'wb'))
-    setup_upload_from_s3(pkl_out_file, recursive=False)
-    # s3_directory = 's3://mousebrainatlas-data/CSHL_cells_dm/'+stack+'/'+structure+'/'
-    # run('aws s3 cp {0} {1}'.format(pkl_out_file,s3_directory))
-    print(structure + '_' + state + ' finished in %5.1f seconds' % (time() - t1))
+    patches = [dir for dir in glob(os.environ['ROOT_DIR'] + patch_dir + '/' + structure + '_surround_500um_noclass/*')]
+    n_choose = min(len(patches), 1000)
+    indices_choose = np.random.choice(range(len(patches)), n_choose, replace=False)
+    patches = np.array(patches)
+    patches_nega = patches[indices_choose]
+    n_neg = len(patches_nega)
+
+    patches_test = np.concatenate((patches_posi, patches_nega))
+    data = (np.array([cv2.imread(dir, 0) for dir in patches_test if cv2.imread(dir, 0).shape == (224, 224)]) - mean_img)[:, None, :, :]
+    del_pos = len([cv2.imread(dir, 0) for dir in patches_posi if cv2.imread(dir, 0).shape != (224, 224)])
+    del_neg = len([cv2.imread(dir, 0) for dir in patches_nega if cv2.imread(dir, 0).shape != (224, 224)])
+    labels = np.r_[np.ones((n_pos - del_pos,)), np.zeros((n_neg - del_neg,))]
+    data_iter = mx.io.NDArrayIter(
+        data=data,
+        batch_size=batch_size,
+        shuffle=False)
+    outputs = model.predict(data_iter, always_output_list=True)
+    features = outputs[0].asnumpy()
+
+    return features, labels
+
+MXNET_ROOTDIR = 'mxnet_models'
+model_dir_name = 'inception-bn-blue'
+model_name = 'inception-bn-blue'
+setup_download_from_s3(os.path.join(MXNET_ROOTDIR, model_dir_name))
+mean_img = np.load(os.path.join(os.environ['ROOT_DIR'], MXNET_ROOTDIR, model_dir_name, 'mean_224.npy'))
+model_prefix = os.path.join(MXNET_ROOTDIR, model_dir_name, model_name)
+
+batch_size = 32
+model0, arg_params, aux_params = mx.model.load_checkpoint(os.path.join(os.environ['ROOT_DIR'],model_prefix), 0)
+output_symbol_name='flatten_output'
+flatten_output = model0.get_internals()[output_symbol_name]
+model = mx.mod.Module(context=mx.cpu(), symbol=flatten_output)
+model.bind(data_shapes=[('data', (batch_size,1,224,224))], for_training=False)
+model.set_params(arg_params=arg_params, aux_params=aux_params, allow_missing=True)
+train_features1, train_labels1 = extract_cnn_features('MD585', structure, batch_size, model)
+train_features2, train_labels2 = extract_cnn_features('MD589', structure, batch_size, model)
+train_features = np.concatenate((train_features1, train_features2))
+train_labels = np.r_[train_labels1, train_labels2]
+clf = XGBClassifier(max_depth=5, learning_rate=0.2, n_estimators=100,
+                            silent=False, objective='binary:logistic')
+clf.fit(train_features, train_labels)
+
+test_features, test_labels = extract_cnn_features('MD594', structure, batch_size, model)
+scores = clf.predict(test_features, output_margin=True)
+pred = scores>0
+acc = sum(pred ==test_labels)/len(test_labels)
+fpr, tpr, threshold = metrics.roc_curve(test_labels, scores)
+roc_auc = metrics.auc(fpr, tpr)
+
+save_dir = os.path.join(os.environ['ROOT_DIR'], MXNET_ROOTDIR, 'inception-xgb_samples')
+if not os.path.exists(save_dir):
+    os.mkdir(save_dir)
+prefix = os.path.join(MXNET_ROOTDIR, 'inception-xgb_samples', structure)
+train_samples = {}
+train_samples['features'] = train_features
+train_samples['labels'] = train_labels
+train_fp = '%s-train.pkl' % prefix
+pickle.dump(train_samples, open(os.environ['ROOT_DIR'] + train_fp, 'wb'))
+setup_upload_from_s3(train_fp, recursive=False)
+
+test_samples = {}
+test_samples['features'] = test_features
+test_samples['labels'] = test_labels
+test_samples['acc'] = acc
+test_samples['auc'] = roc_auc
+test_fp = '%s-test.pkl' % prefix
+pickle.dump(test_samples, open(os.environ['ROOT_DIR'] + test_fp, 'wb'))
+setup_upload_from_s3(test_fp, recursive=False)
+
+
+
+
 
---
+++ scripts/Cell_datajoint/Inception-xgb_datajoint.py
@@ -0,0 +1,70 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("Environment", type=str, help="Local or AWS")
+args = parser.parse_args()
+
+import datajoint as dj
+import numpy as np
+import json
+from subprocess import call
+import yaml
+import sys, os
+import shutil
+import pandas as pd
+import ray
+
+sys.path.append('./lib')
+from utilities import *
+sys.path.append('../lib')
+from utils import run
+
+if args.Environment == 'AWS':
+    credFiles= '/home/ubuntu/data/Github/VaultBrain/credFiles_aws.yaml'
+    yaml_file = 'shape_params-aws.yaml'
+else:
+    credFiles= '/Users/kuiqian/Github/VaultBrain/credFiles.yaml'
+    yaml_file = 'shape_params.yaml'
+
+dj.config['database.host'] = get_dj_creds(credFiles)['database.host']
+dj.config['database.user'] = get_dj_creds(credFiles)['database.user']
+dj.config['database.port'] = get_dj_creds(credFiles)['database.port']
+dj.config['database.password'] = get_dj_creds(credFiles)['database.password']
+dj.conn()
+
+schema = dj.schema('kui_diffusionmap')
+schema.spawn_missing_classes()
+
+
+MXNET_ROOTDIR = 'mxnet_models'
+
+scripts_dir = os.environ['REPO_DIR']
+
+
+@schema
+class CnnTraining(dj.Computed):
+    definition="""
+    -> Structure
+    -----
+    size_of_file : int   #size of eval Metric History file
+    """
+
+    bucket = "mousebrainatlas-data"
+    client = get_s3_client(credFiles)
+    def make(self, key):
+        structure = (Structure & key).fetch1('structure')
+        if structure == '7nn':
+            structure = '7n'
+        print('populating for ', structure, end='\n')
+        prefix = os.path.join(MXNET_ROOTDIR, 'inception-xgb_samples', structure)
+        s3_fp = prefix + '-train.pkl'
+        print(s3_fp)
+        try:
+            report = self.client.stat_object(self.bucket, s3_fp)
+        except:
+            run('python3 {0}/CNN_training.py {1}'.format(scripts_dir, structure))
+            report = self.client.stat_object(self.bucket, s3_fp)
+        key['size_of_file'] = int(report.size)
+        self.insert1(key)
+
+CnnTraining.populate(suppress_errors=True, reserve_jobs=True)
+
